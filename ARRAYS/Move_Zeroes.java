ffd2129a00dbcc47b51867652bc615cadb221fe2
VidhanThakur09 <vidhanthakur2700@gmail.com>
2025-08-21 23:30:51 +0530

    Day 4

---
+++ ARRAYS/Move_Zeroes.java
@@ -0,0 +1,18 @@
+public class Move_Zeroes{
+
+    public static void moveZeroes(int[] nums) {
+        int i = 0;
+        for(int j = 0;j<nums.length;j++){
+            if(nums[j]!=0){
+                nums[i++] = nums[j];
+            }
+        }
+        while(i<nums.length){
+            nums[i++] = 0;
+        }
+    }
+    public static void main (String arg[]){
+        int nums[] = {0,1,0,3,12};
+        moveZeroes(nums);
+    }
+}
