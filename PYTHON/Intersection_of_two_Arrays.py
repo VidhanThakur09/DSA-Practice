507ee3255cc37ed5a7ace0f9ea24e70244497f5a
aditid-hub <aditid155@gmail.com>
2025-08-29 19:47:25 +0530

    Aditi push

---
+++ PYTHON/Intersection_of_two_Arrays.py
@@ -0,0 +1,21 @@
+"""
+Problem No. 349. Intersection of Two Arrays:
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+Example:
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+"""
+
+def intersection(nums1,nums2):
+    set1= set(nums1)
+    set2= set(nums2)
+    
+    return list((set1&set2))
+
+nums1 = [4,9,5]
+nums2= [9,4,9,8,4]
+print(list(set(nums1) & set(nums2)))
+
+
+
