bd25fcbf56d754212b41cdcc819c96075e9fef5c
aditid-hub <aditid155@gmail.com>
2025-08-31 01:15:31 +0530

    Aditi pulling request

---
+++ PYTHON/Duplicate_Zeroes.py
@@ -0,0 +1,43 @@
+"""
+1089. Duplicate Zeros:
+Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
+
+Note that elements beyond the length of the original array are not written. Do the above modifications to the input array in place and do not return anything.
+
+Input: arr = [1,0,2,3,0,4,5,0]
+Output: [1,0,0,2,3,0,0,4]
+Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+"""
+
+def duplicateZeros(arr:list[int]) -> None:
+        zeroes = arr.count(0)
+        n = len(arr)
+        for i in range(n-1, -1, -1):
+            if i + zeroes < n:
+                arr[i + zeroes] = arr[i]
+            if arr[i] == 0: 
+                zeroes -= 1
+                if i + zeroes < n:
+                    arr[i + zeroes] = 0
+     
+arr = [1,0,2,3,0,4,5,0]
+print(duplicateZeros(arr))
+for i in range (0,len(arr)) :
+     print(arr[i])
+    
+      
+
+
+      
+        
+      
+      
+      
+       
+         
+    
+         
+
+
+
+
