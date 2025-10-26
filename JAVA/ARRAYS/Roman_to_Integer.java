719d9b95fe022b7cb07bba00ed4769a7c7d2017b
VidhanThakur09 <vidhanthakur2700@gmail.com>
2025-10-26 21:06:39 +0530

    Day 18

---
+++ JAVA/ARRAYS/Roman_to_Integer.java
@@ -0,0 +1,23 @@
+public class Roman_to_Integer{
+    public int romanToInt(String s) {
+        int result = 0,prev = 0, curr = 0;
+        for(int i = s.length()-1;i>=0;i--){
+            switch(s.charAt(i)){
+                case 'I':curr = 1;break;
+                case 'V':curr = 5;break;
+                case 'X':curr = 10;break;
+                case 'L':curr = 50;break;
+                case 'C':curr = 100;break;
+                case 'D':curr = 500;break;
+                case 'M':curr = 1000;break;
+            }
+            if(prev>curr){
+                result-=curr;
+            }else{
+                result+=curr;
+            }
+            prev = curr;
+        }
+        return result;
+    }
+}
