1aa034d07278df05b649da2e59278776a2f1d14e
unknown <aditid155@gmail.com>
2025-12-14 00:02:38 +0530

    Aditi pulling PR

---
+++ PYTHON/Arrays/Missing_number.py
@@ -0,0 +1,17 @@
+"""
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+ 
+"""
+def missingnumbers(nums) :
+    seen = set()
+    for i in range (0,n) :
+        if nums[i] not in seen :
+            print(set.add(nums[i]))
+            print(seen)
+nums = [4,3,2,7,8,2,3,1]
+print(def missingnumbers)
+#Output: [5,6]
+
+       
+            
